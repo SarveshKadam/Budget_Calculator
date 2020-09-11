6e696882fcd5109c6305ea59ecc6b30c6cb9db09
unknown <kadamsarvesh1905@gmail.com>
2020-09-11 18:49:23 +0530

    income added

--- app.js
+++ app.js
@@ -24,6 +24,27 @@ yargs.command({
     }
 })
 
+//----------- ADD INCOME -----------
+yargs.command({
+    command : "add-income",
+    describe:"Adding Income",
+    builder:{
+        title:{
+            describe: "Add a title",
+            demandOption: true,
+            type:'string'
+        },
+        amount:{
+            describe:"Add the income amount",
+            demandOption:true,
+            type:'number'
+        }
+    },
+    handler : (argv)=> {
+        budget.addIncome(argv.title,argv.amount)
+    }
+})
+
 //-------------- REMOVE EXPENSE -----------
 
 yargs.command({
@@ -41,15 +62,54 @@ yargs.command({
     }
 })
 
+//-------------- REMOVE INCOME -----------
+
+yargs.command({
+    command: "remove-income",
+    describe:"Remove the Income",
+    builder:{
+        title: {
+            describe:"Add a title",
+            demandOption :true,
+            type:"string"
+        }
+    },
+    handler:(argv)=>{
+        budget.removeIncome(argv.title)
+    }
+})
+
 
 
 //------------- TOTAL EXPENSE---------------
 
 yargs.command({
-    command:"total",
+    command:"total-expense",
     describe:"Total expense",
     handler :()=>{
         budget.totalExpense()
+    }
+
+})
+
+//------------- TOTAL INCOME---------------
+
+yargs.command({
+    command:"total-income",
+    describe:"Total income",
+    handler :()=>{
+        budget.totalIncome()
+    }
+
+})
+
+//------------- BUDGET CALCULATION--------------
+
+yargs.command({
+    command:"budget",
+    describe:"Total Budget",
+    handler :()=>{
+        budget.calculateBudget()
     }
 
 })
--- budget.js
+++ budget.js
@@ -1,10 +1,10 @@
 const fs =require('fs')
 const chalk = require('chalk')
-const { exitProcess } = require('yargs')
+const yargs = require('yargs')
 
 //Add expense
 const addExpense = (title,amount)=>{
-    const budget = loadbudget()
+    const budget = loadExpense()
     const duplicatetitle = budget.find((item)=>{
         return item.title === title
     })
@@ -15,16 +15,37 @@ const addExpense = (title,amount)=>{
             title :title,
             amount:amount
         })
-        savebudget(budget)
+        saveExpense(budget)
         console.log(chalk.green.inverse("Item added to the list"))
     }else{
-        console.log(chalk.red.inverse("Item with same title is laredy present"))
+        console.log(chalk.red.inverse("Item with same title is alredy present"))
     }
 }
 
+//Add income
+const addIncome = (title,amount)=>{
+    const budget = loadIncome()
+    const duplicatetitle = budget.find((item)=>{
+        return item.title === title
+    })
+
+    if(!duplicatetitle){
+
+        budget.push({
+            title :title,
+            amount:amount
+        })
+        saveIncome(budget)
+        console.log(chalk.green.inverse("Item added to the list"))
+    }else{
+        console.log(chalk.red.inverse("Item with same title is alredy present"))
+    }
+}
+
+
 //Remove expense
 const removeExpense = (title)=>{
-    const budget = loadbudget()
+    const budget = loadExpense()
     const removetitle = budget.find((item)=>{
         return item.title === title
     })
@@ -33,7 +54,25 @@ const removeExpense = (title)=>{
         if(index > -1){
             budget.splice(index,1)
         }
-        savebudget(budget)
+        saveExpense(budget)
+        console.log(chalk.green.inverse("Item has been removed"))
+    }else{
+        console.log(chalk.red.inverse("There is no such item"))
+    }
+}
+
+//Remove INCOME
+const removeIncome = (title)=>{
+    const budget = loadIncome()
+    const removetitle = budget.find((item)=>{
+        return item.title === title
+    })
+    if(removetitle){
+        const index = budget.indexOf(removetitle)
+        if(index > -1){
+            budget.splice(index,1)
+        }
+        saveIncome(budget)
         console.log(chalk.green.inverse("Item has been removed"))
     }else{
         console.log(chalk.red.inverse("There is no such item"))
@@ -42,18 +81,35 @@ const removeExpense = (title)=>{
 
 //Total expense
 const totalExpense = ()=>{
-    const expense = loadbudget();
-    const total = expense.reduce((sum,element)=>{
-        return sum + element.amount
-    },0)
+    const total = totalExpenseAmount()
     console.log("Total expense amount is "+chalk.yellow.inverse(total))
     
 }
 
 
-const loadbudget = ()=>{
+
+//Total Income
+const totalIncome = ()=>{
+    const total = totalIncomeAmount()
+    console.log("Total Income amount is "+chalk.yellow.inverse(total))
+    
+}
+
+
+//CALCULATE BUDGET
+const calculateBudget = ()=>{
+    const income = totalIncomeAmount()
+    const expense = totalExpenseAmount()
+
+    const budget = income - expense
+    console.log("Total Budget calculated is "+chalk.green(budget))
+}
+
+
+//---------------BASIC---------------
+const loadExpense = ()=>{
     try {
-        const dataBuffer = fs.readFileSync('./budget.json')
+        const dataBuffer = fs.readFileSync('./expense.json')
         const dataJSON = dataBuffer.toString()
         return JSON.parse(dataJSON)
         
@@ -63,13 +119,49 @@ const loadbudget = ()=>{
     
 }
 
-const savebudget = (item)=>{
+const saveExpense = (item)=>{
     const JSONdata = JSON.stringify(item)
-    fs.writeFileSync('./budget.json',JSONdata)
+    fs.writeFileSync('./expense.json',JSONdata)
 }
+
+const loadIncome = ()=>{
+    try {
+        const dataBuffer = fs.readFileSync('./income.json')
+        const dataJSON = dataBuffer.toString()
+        return JSON.parse(dataJSON)
+        
+    } catch (error) {
+        return []
+    }
+    
+}
+
+const saveIncome = (item)=>{
+    const JSONdata = JSON.stringify(item)
+    fs.writeFileSync('./income.json',JSONdata)
+}
+
+const totalExpenseAmount = ()=>{
+    const expense = loadExpense();
+    return expense.reduce((sum,element)=>{
+        return sum + element.amount
+    },0)
+}
+
+const totalIncomeAmount = ()=>{
+    const Income = loadIncome();
+    return Income.reduce((sum,element)=>{
+        return sum + element.amount
+    },0)
+}
+
 
 module.exports ={
     addExpense : addExpense,
     totalExpense :totalExpense,
-    removeExpense:removeExpense
+    removeExpense:removeExpense,
+    addIncome:addIncome,
+    totalIncome:totalIncome,
+    calculateBudget:calculateBudget,
+    removeIncome:removeIncome
 }
